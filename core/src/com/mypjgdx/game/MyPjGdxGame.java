d496f0b16cecc751909c6d760b934d07fbd0d796
S. Kyousuke <surasek@gmail.com>
2015-11-20 00:38:27 +0700

    Edit Tiling System

--- core/src/com/mypjgdx/game/MyPjGdxGame.java
+++ core/src/com/mypjgdx/game/MyPjGdxGame.java
@@ -5,6 +5,9 @@ import java.util.List;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Pixmap;
+import com.badlogic.gdx.graphics.Pixmap.Format;
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
@@ -53,6 +56,6 @@ public class MyPjGdxGame extends ApplicationAdapter {
 	    default:
 	        break;
 	    }
-	    return tileAtlas.findRegion("blue");
+	    return new TextureRegion(new Texture(new Pixmap(0, 0, Format.Alpha)));
 	}
 }
