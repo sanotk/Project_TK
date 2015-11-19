339adc72d74754376eb9cd96896979fa0f790dd4
sanotk <sanomarutk@gmail.com>
2015-11-20 00:51:31 +0700

    commit v2

--- core/src/com/mypjgdx/game/MyPjGdxGame.java
+++ core/src/com/mypjgdx/game/MyPjGdxGame.java
@@ -1,19 +1,27 @@
 package com.mypjgdx.game;
 
+import java.util.List;
+
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.utils.IntArray;
 
 public class MyPjGdxGame extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture img;
-	
+
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
 		img = new Texture("badlogic.jpg");
+
+		List<IntArray> maplayer1 = LevelLoader.loadMap(Gdx.files.internal("mix_map1.csv"));
+		List<IntArray> maplayer2 = LevelLoader.loadMap(Gdx.files.internal("mix_map2.csv"));
+		System.out.println(maplayer1);
+		System.out.println(maplayer2);
 	}
 
 	@Override
