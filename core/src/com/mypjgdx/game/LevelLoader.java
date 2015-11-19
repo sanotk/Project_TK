2f38cabd0ddd2ebf7bc3e4c56d2f39220d1978dd
S. Kyousuke <surasek@gmail.com>
2015-11-20 00:31:20 +0700

    Add tiling sytem

---
+++ core/src/com/mypjgdx/game/LevelLoader.java
@@ -0,0 +1,26 @@
+package com.mypjgdx.game;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.badlogic.gdx.files.FileHandle;
+import com.badlogic.gdx.utils.IntArray;
+
+public class LevelLoader {
+
+    public static List<IntArray> loadMap(FileHandle mapFile) {
+
+        List<IntArray> mapData = new ArrayList<IntArray>();
+        String mapStringData = mapFile.readString();
+
+        for (String rowString : mapStringData.split("\\r?\\n")) {
+            IntArray rowInt = new IntArray();
+            for (String tileNum : rowString.split(",")) {
+                rowInt.add(Integer.parseInt(tileNum));
+            }
+            mapData.add(0, rowInt);
+        }
+        return mapData;
+    }
+
+}
--- core/src/com/mypjgdx/game/MyPjGdxGame.java
+++ core/src/com/mypjgdx/game/MyPjGdxGame.java
@@ -1,27 +1,58 @@
 package com.mypjgdx.game;
 
+import java.util.List;
+
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.utils.IntArray;
 
 public class MyPjGdxGame extends ApplicationAdapter {
 	SpriteBatch batch;
-	Texture img;
-	
+	TextureAtlas tileAtlas;
+	List<IntArray> mapData;
+
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		tileAtlas = new TextureAtlas(Gdx.files.internal("tile.atlas"));
+		mapData = LevelLoader.loadMap(Gdx.files.internal("map.csv"));
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
+		Gdx.gl.glClearColor(0, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+		int x = 0, y = 0;
 		batch.begin();
-		batch.draw(img, 0, 0);
+		for (int row = 0; row < mapData.size(); ++row) {
+		    for (int column = 0; column < mapData.get(row).size; ++column) {
+		        batch.draw(findTileRegion(mapData.get(row).get(column)), x, y);
+		        x += 32;
+		    }
+		    x = 0;
+		    y += 32;
+		}
 		batch.end();
+	}
+
+	public TextureRegion findTileRegion(int tileNum) {
+	    switch(tileNum) {
+	    case 0:
+	        return tileAtlas.findRegion("blue");
+	    case 1:
+            return tileAtlas.findRegion("green");
+	    case 2:
+            return tileAtlas.findRegion("red");
+	    case 3:
+            return tileAtlas.findRegion("yellow");
+	    default:
+	        break;
+	    }
+	    return tileAtlas.findRegion("blue");
 	}
 }
