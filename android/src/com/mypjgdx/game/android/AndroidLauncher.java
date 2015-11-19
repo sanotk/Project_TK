8914e03f2fc97200e1af9f44686bfa4a53aef797
sanotk <sanomarutk@gmail.com>
2015-11-19 22:19:03 +0700

    คอมมิตที่ 1

---
+++ android/src/com/mypjgdx/game/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.mypjgdx.game.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.mypjgdx.game.MyPjGdxGame;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new MyPjGdxGame(), config);
+	}
+}
