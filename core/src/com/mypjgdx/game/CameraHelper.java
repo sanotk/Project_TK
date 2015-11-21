40f4d679f903045317ffe56860e78ac3927137a5
sanotk <sanomarutk@gmail.com>
2015-11-21 13:49:08 +0700

    CameraHelper

--- core/src/com/mypjgdx/game/CameraHelper.java
+++ core/src/com/mypjgdx/game/CameraHelper.java
@@ -6,46 +6,46 @@ import com.badlogic.gdx.math.Vector2;
 
 public class CameraHelper {
 
-    private final float MAX_ZOOM_IN = 0.25f;
-    private final float MAX_ZOOM_OUT = 10.0f;
+    private final float MAX_ZOOM_IN = 0.25f; //ตัวแปรอัตราการซูมเข้าสูงสุด
+    private final float MAX_ZOOM_OUT = 10.0f; //ตัวแปรอัตราการซูมออกสูงสุด
 
-    private Vector2 position;
-    private float zoom;
+    private Vector2 position; //ตำแหน่ง 2 มิติ
+    private float zoom; //ตัวแปรซูม
 
-    public CameraHelper () {
+    public CameraHelper () { //เรียกใช้ ตำแหน่ง 2 มิติ เก็บไว้ในตัวแปร position และเซ็ตค่า zoom
         position = new Vector2();
         zoom = 1.0f;
     }
 
-    public void setPosition (float x, float y) {
+    public void setPosition (float x, float y) { //เซ็ตตำแหน่ง
         this.position.set(x, y);
     }
 
-    public void addPostion(float x, float y) {
+    public void addPostion(float x, float y) { //เพิ่มตำแหน่ง
         this.position.add(x, y);
     }
 
-    public Vector2 getPosition () {
+    public Vector2 getPosition () { // เรียกค่า position ปัจจุบัน
         return position;
     }
 
-    public void addZoom (float amount) {
+    public void addZoom (float amount) { //รับค่า amount แล้วเซ็ตเพิ่มค่าซูมเป็นเท่านั้น
         setZoom(zoom + amount);
     }
 
-    public void setZoom (float zoom) {
+    public void setZoom (float zoom) { //เซ็ตค่าซูม
         this.zoom = MathUtils.clamp(zoom, MAX_ZOOM_IN, MAX_ZOOM_OUT);
     }
 
-    public float getZoom () {
+    public float getZoom () { // เรียกค่า zoom ในปัจจุบัน
         return zoom;
     }
 
-    public void applyTo (OrthographicCamera camera) {
-        camera.position.x = position.x;
-        camera.position.y = position.y;
-        camera.zoom = zoom;
-        camera.update();
+    public void applyTo (OrthographicCamera camera) { //อัพเทดตำแหน่ง
+        camera.position.x = position.x; //
+        camera.position.y = position.y; //
+        camera.zoom = zoom; //
+        camera.update(); //
     }
 
 }
