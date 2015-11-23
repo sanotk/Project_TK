76bacab838e06de60064b27d8f525a2389f5f5f8
S. Kyousuke <surasek@gmail.com>
2015-11-24 02:13:27 +0700

    Add android input control

--- core/src/com/mypjgdx/game/WorldController.java
+++ core/src/com/mypjgdx/game/WorldController.java
@@ -4,7 +4,6 @@ import com.badlogic.gdx.Application.ApplicationType;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input.Keys;
 import com.badlogic.gdx.InputAdapter;
-import com.badlogic.gdx.InputMultiplexer;
 import com.badlogic.gdx.input.GestureDetector;
 import com.badlogic.gdx.input.GestureDetector.GestureListener;
 import com.badlogic.gdx.math.Vector2;
@@ -18,18 +17,16 @@ public class WorldController extends InputAdapter implements GestureListener  {
     private final CameraHelper cameraHelper;
 
     private final Sano sano;
-    private String message = "No gesture performed yet";
 
     public WorldController(Sano sano) {
         this.sano = sano;
         cameraHelper = new CameraHelper();
-        Gdx.input.setInputProcessor(this);
-        InputMultiplexer im = new InputMultiplexer();
-        GestureDetector gd = new GestureDetector(this);
-        im.addProcessor(gd);
-        im.addProcessor(this);
-        Gdx.input.setInputProcessor(im);
+        GestureDetector gestureDetector = new GestureDetector(this);
 
+        if (Gdx.app.getType() == ApplicationType.Android){
+            Gdx.input.setInputProcessor(gestureDetector);
+        }
+        else  Gdx.input.setInputProcessor(this);
     }
 
     public CameraHelper getCameraHelper() { return cameraHelper; }
@@ -47,6 +44,7 @@ public class WorldController extends InputAdapter implements GestureListener  {
 
     private void handleCameraInput(float deltaTime) {
         if (cameraHelper.hasTarget()) return; // มุมกล้องติดตาม Sano อยู่จะใช้ Input เลื่อนมุมกล้องเองไม่ได้
+
         if (Gdx.app.getType() != ApplicationType.Desktop) return; // ปุ่มกดสำหรับแฟลตฟอร์ม Desktop เท่านั้น
 
         if (Gdx.input.isKeyPressed(Keys.UP)) cameraHelper.addPostion(0, CAMERA_SPEED * deltaTime); //กดลูกศรขึ้น
@@ -59,21 +57,36 @@ public class WorldController extends InputAdapter implements GestureListener  {
 
     private void handleSanoInput()  { // ควบคุม Sano
         if (!cameraHelper.hasTarget()) return; // มุมกล้องติดตาม Sano อยู่ถึงจะควมคุม Sano ได้
-        if (Gdx.app.getType() != ApplicationType.Desktop) return; // ปุ่มกดสำหรับแฟลตฟอร์ม Desktop เท่านั้น
 
         final float SANO_SPEED = 100.0f;
-        if (Gdx.input.isKeyPressed(Keys.UP)) sano.velocity.y = SANO_SPEED ;       //กดลูกศรขึ้น
-        if (Gdx.input.isKeyPressed(Keys.DOWN)) sano.velocity.y = -SANO_SPEED ;      //กดลูกศรลง
-        if (Gdx.input.isKeyPressed(Keys.LEFT)) sano.velocity.x = -SANO_SPEED ;      //กดลูกศรซ้าย
-        if (Gdx.input.isKeyPressed(Keys.RIGHT)) sano.velocity.x = SANO_SPEED ;     //กดลูกศรขวา
+
+        if (Gdx.app.getType() == ApplicationType.Android && Gdx.input.isTouched())  {
+            int screenWidth = Gdx.graphics.getWidth();
+            int screenHeight = Gdx.graphics.getHeight();
+            float x = Gdx.input.getX();
+            float filppedY = screenHeight- Gdx.input.getY();
+            final float SCREEN_MOVE_EGDE = 0.4f;
+
+            if (x > screenWidth * (1.0f - SCREEN_MOVE_EGDE)) sano.velocity.x = SANO_SPEED;
+            else if (x < screenWidth * SCREEN_MOVE_EGDE )  sano.velocity.x = -SANO_SPEED;
+
+            if (filppedY > screenHeight * (1.0f - SCREEN_MOVE_EGDE)) sano.velocity.y = SANO_SPEED;
+            else if (filppedY < screenHeight * SCREEN_MOVE_EGDE)  sano.velocity.y = -SANO_SPEED;
+        }
+        else {
+            if (Gdx.input.isKeyPressed(Keys.UP)) sano.velocity.y = SANO_SPEED ;       //กดลูกศรขึ้น
+            if (Gdx.input.isKeyPressed(Keys.DOWN)) sano.velocity.y = -SANO_SPEED ;      //กดลูกศรลง
+            if (Gdx.input.isKeyPressed(Keys.LEFT)) sano.velocity.x = -SANO_SPEED ;      //กดลูกศรซ้าย
+            if (Gdx.input.isKeyPressed(Keys.RIGHT)) sano.velocity.x = SANO_SPEED ;     //กดลูกศรขวา
+        }
     }
 
     @Override
     public boolean keyDown(int keycode) {
         if (keycode == Keys.R) {    // กด R เพื่อ Reset มุมกล้อง ยกเลิกการมุมกล้องติดตาม Sano
             cameraHelper.setPosition(0, 0);
-            cameraHelper.setZoom(1.0f);
-            cameraHelper.setTarget(null);
+            cameraHelper.setZoom(0.8f);
+            sano.init();
         }
         else if (keycode == Keys.SPACE) { // กด Spacebar เพื่อให้มุมกล้องติดตาม/เลิกติดตาม Sano
             if (!cameraHelper.hasTarget()) cameraHelper.setTarget(sano);
@@ -82,61 +95,52 @@ public class WorldController extends InputAdapter implements GestureListener  {
         return false;
     }
 
-	@Override
-	public boolean touchDown(float x, float y, int pointer, int button) {
-        message = "Touch down!";
-        Gdx.app.log("INFO", message);
+    @Override
+    public boolean touchDown(float x, float y, int pointer, int button) {
         return true;
-	}
+    }
 
-	@Override
-	public boolean tap(float x, float y, int count, int button) {
-        message = "Tap performed, finger" + Integer.toString(button);
-        Gdx.app.log("INFO", message);
+    @Override
+    public boolean tap(float x, float y, int count, int button) {
+        if (count == 2) {
+            if (!cameraHelper.hasTarget()) cameraHelper.setTarget(sano);
+            else {
+                cameraHelper.setPosition(0, 0);
+                cameraHelper.setZoom(0.8f);
+                sano.init();
+            }
+        }
         return false;
-	}
+    }
 
-	@Override
-	public boolean longPress(float x, float y) {
-        message = "Long press performed";
-        Gdx.app.log("INFO", message);
+    @Override
+    public boolean longPress(float x, float y) {
         return true;
-	}
+    }
 
-	@Override
-	public boolean fling(float velocityX, float velocityY, int button) {
-        message = "Fling performed, velocity:" + Float.toString(velocityX) +
-                "," + Float.toString(velocityY);
-        Gdx.app.log("INFO", message);
+    @Override
+    public boolean fling(float velocityX, float velocityY, int button) {
         return true;
-	}
+    }
 
-	@Override
-	public boolean pan(float x, float y, float deltaX, float deltaY) {
-        message = "Pan performed, delta:" + Float.toString(deltaX) +
-                "," + Float.toString(deltaY);
-        Gdx.app.log("INFO", message);
+    @Override
+    public boolean pan(float x, float y, float deltaX, float deltaY) {
         return true;
-	}
+    }
 
-	@Override
-	public boolean panStop(float x, float y, int pointer, int button) {
-		// TODO Auto-generated method stub
-		return false;
-	}
+    @Override
+    public boolean panStop(float x, float y, int pointer, int button) {
+        return false;
+    }
 
-	@Override
-	public boolean zoom(float initialDistance, float distance) {
-        message = "Zoom performed, initial Distance:" + Float.toString(initialDistance) +
-                " Distance: " + Float.toString(distance);
-        Gdx.app.log("INFO", message);
+    @Override
+    public boolean zoom(float initialDistance, float distance) {
         return true;
-	}
+    }
 
-	@Override
-	public boolean pinch(Vector2 initialPointer1, Vector2 initialPointer2, Vector2 pointer1, Vector2 pointer2) {
-		// TODO Auto-generated method stub
-		return false;
-	}
+    @Override
+    public boolean pinch(Vector2 initialPointer1, Vector2 initialPointer2, Vector2 pointer1, Vector2 pointer2) {
+        return false;
+    }
 
 }
