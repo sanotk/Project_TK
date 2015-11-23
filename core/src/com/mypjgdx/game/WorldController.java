9f4f31099412dde21204ee837148f3bb9467f7ac
sanotk <sanomarutk@gmail.com>
2015-11-23 09:43:37 +0700

    no comment

--- core/src/com/mypjgdx/game/WorldController.java
+++ core/src/com/mypjgdx/game/WorldController.java
@@ -76,6 +76,8 @@ public class WorldController extends InputAdapter implements GestureListener  {
 	@Override
 	public boolean touchDown(float x, float y, int pointer, int button) {
 		// TODO Auto-generated method stub
+
+
 		return false;
 	}
 
