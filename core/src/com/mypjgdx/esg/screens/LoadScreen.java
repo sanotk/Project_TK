3c8ed16ed9fedc963570a28c67a9959c04ba9f54
sanotk <sanomarutk@gmail.com>
2015-12-22 15:48:24 +0700

    New name and Main Menu Demo

---
+++ core/src/com/mypjgdx/esg/screens/LoadScreen.java
@@ -0,0 +1,35 @@
+package com.mypjgdx.esg.screens;
+
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input.Keys;
+import com.badlogic.gdx.graphics.GL20;
+
+public class LoadScreen extends AbstractGameScreen {
+
+    public LoadScreen(Game game) {
+        super(game);
+    }
+
+    @Override
+    public void render(float deltaTime) {
+        Gdx.gl.glClearColor(0.1f, 0.2f, 0.4f, 1.0f);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+        if(Gdx.input.isKeyJustPressed(Keys.SPACE))
+            game.setScreen(new MenuScreen(game));
+    }
+
+    @Override
+    public void resize(int width, int height) {}
+
+    @Override
+    public void show() {}
+
+    @Override
+    public void hide() {}
+
+    @Override
+    public void pause() {}
+
+}
--- core/src/com/mypjgdx/esg/screens/MenuScreen.java
+++ core/src/com/mypjgdx/esg/screens/MenuScreen.java
@@ -2,26 +2,102 @@ package com.mypjgdx.esg.screens;
 
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.Input.Keys;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 
 public class MenuScreen extends AbstractGameScreen {
 
-    public MenuScreen(Game game) {
+    public static final int SCENE_WIDTH = 1280; //เซตค่าความกว้างของจอ
+    public static final int SCENE_HEIGHT = 720; //เซตค่าความสูงของจอ
+    private Stage stage;
+    private Skin skin;
+
+    public MenuScreen(final Game game) {
         super(game);
+
+        stage = new Stage();
+        Gdx.input.setInputProcessor(stage);
+
+        skin = new Skin(Gdx.files.internal("uiskin.json"));
+
+        int btn_w = 200;
+        int btn_h = 50;
+
+        TextButton buttonStart = new TextButton("START", skin);
+        buttonStart.setWidth(btn_w);
+        buttonStart.setHeight(btn_h);
+        buttonStart.setPosition(SCENE_WIDTH / 2 - btn_w / 2, 450);
+
+        TextButton buttonLoad = new TextButton("LOAD", skin);
+        buttonLoad.setWidth(btn_w);
+        buttonLoad.setHeight(btn_h);
+        buttonLoad.setPosition(SCENE_WIDTH / 2 - btn_w / 2, 380);
+
+        TextButton buttonOption = new TextButton("OPTION", skin);
+        buttonOption.setWidth(btn_w);
+        buttonOption.setHeight(btn_h);
+        buttonOption.setPosition(SCENE_WIDTH / 2 - btn_w / 2, 310);
+
+        TextButton buttonExit = new TextButton("EXIT", skin);
+        buttonExit.setWidth(btn_w);
+        buttonExit.setHeight(btn_h);
+        buttonExit.setPosition(SCENE_WIDTH / 2 - btn_w / 2, 240);
+
+        stage.addActor(buttonStart);
+        stage.addActor(buttonLoad);
+        stage.addActor(buttonOption);
+        stage.addActor(buttonExit);
+
+        buttonStart.addListener(new ClickListener() {
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+                super.clicked(event, x, y);
+                game.setScreen(new GameScreen(game));
+            }
+        });
+
+        buttonLoad.addListener(new ClickListener() {
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+                super.clicked(event, x, y);
+                game.setScreen(new LoadScreen(game));
+            }
+        });
+
+        buttonOption.addListener(new ClickListener() {
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+                super.clicked(event, x, y);
+                game.setScreen(new OptionScreen(game));
+            }
+        });
+
+        buttonExit.addListener(new ClickListener() {
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+                super.clicked(event, x, y);
+
+            }
+        });
+
     }
 
     @Override
     public void render(float deltaTime) {
         Gdx.gl.glClearColor(0.1f, 0.2f, 0.4f, 1.0f);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-
-        if(Gdx.input.isKeyJustPressed(Keys.SPACE))
-            game.setScreen(new GameScreen(game));
+        stage.act(Gdx.graphics.getDeltaTime());
+        stage.draw();
     }
 
     @Override
-    public void resize(int width, int height) {}
+    public void resize(int width, int height) {
+    	stage.getViewport().update(width, height, true);
+    }
 
     @Override
     public void show() {}
@@ -32,4 +108,9 @@ public class MenuScreen extends AbstractGameScreen {
     @Override
     public void pause() {}
 
+    @Override
+    public void dispose() {
+        stage.dispose();
+        skin.dispose();
+    }
 }
