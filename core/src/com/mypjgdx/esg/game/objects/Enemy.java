6268d755886013b171328065be38322ccb75e161
sanotk <sanomarutk@gmail.com>
2016-02-21 18:30:36 +0700

    no message

--- core/src/com/mypjgdx/esg/game/objects/Enemy.java
+++ core/src/com/mypjgdx/esg/game/objects/Enemy.java
@@ -136,7 +136,7 @@ public class Enemy extends AbstractGameObject {
         	else if(player.bounds.x < bounds.x){
         		player.position.x = player.position.x -= player.bounds.width;
         	}
-        	else if(player.bounds.y < bounds.y){
+        	else if(player.bounds.y > bounds.y){
         		player.position.y = player.position.y += player.bounds.height;
         	}
         	else if(player.bounds.y < bounds.y){
