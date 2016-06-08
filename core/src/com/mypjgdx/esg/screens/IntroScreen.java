a24cb02c4f637360187c65f5192be0572404bc2c
S. Kyousuke <surasek@gmail.com>
2016-06-08 23:44:12 +0700

    Change Dialog System!

--- core/src/com/mypjgdx/esg/screens/IntroScreen.java
+++ core/src/com/mypjgdx/esg/screens/IntroScreen.java
@@ -8,11 +8,11 @@ import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.Texture.TextureFilter;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
-import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.utils.Align;
+import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.utils.viewport.FitViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
+import com.mypjgdx.esg.ui.Dialog;
 
 public class IntroScreen extends AbstractGameScreen {
 
@@ -22,24 +22,22 @@ public class IntroScreen extends AbstractGameScreen {
     private OrthographicCamera camera;
     private SpriteBatch batch;
 	private Viewport viewport;
+    private Stage stage;
 
 	private BitmapFont font;
 	private Texture dialogBackground;
-	private Sprite dialog;
+	private Dialog dialog;
 
 	private String text =
 		        "\"ในปี พ.ศ.2600 ได้เกิดสงครามโลกครั้งที่ 3\" \n\"โดยมีการใช้อาวุธนิวเคลียร์ ทำให้ทั้งโลกเกิดสภาวะอากาศเป็นพิษ\" \n\"ทำให้ผู้คนไม่สามารถอาศัยอยู่บนพื้นโลก ผู้คนจึงต้องหลบไปอยู่ในสถานที่หลบภัย\""
 		        + "\n\"คุณคือผู้กล้าที่ถูกคัดเลือกโดยต้องไปสำรวจสถานที่หลบภัยแห่งหนึ่ง\"\n\"และพาคนในพื้นที่เข้าไปหลบภัยในสถานที่หลบภัยแห่งนั้นให้มากที่สุด \"\n\"เอาล่ะ รีบเร่งมือกันเถอะ (กด Enter เพื่อเริ่มเกม)\"";
-
-	private int charCount = 0;
-	private float stringCompleteness = 0;
-
 
     public IntroScreen(final Game game) {
         super(game);
         camera = new OrthographicCamera();
         viewport = new FitViewport(SCENE_WIDTH, SCENE_HEIGHT, camera);
 		batch = new SpriteBatch();
+		stage = new Stage(viewport, batch);
 
 		font = new BitmapFont(Gdx.files.internal("thai24.fnt"));
 		font.getRegion().getTexture().setFilter(TextureFilter.Linear, TextureFilter.Linear);
@@ -48,9 +46,15 @@ public class IntroScreen extends AbstractGameScreen {
 		dialogBackground = new Texture("dialog.png");
 		dialogBackground.setFilter(TextureFilter.Linear, TextureFilter.Linear);
 
-		dialog = new Sprite(dialogBackground);
-		dialog.setPosition(-dialogBackground.getWidth()*0.5f, -dialogBackground.getHeight()*0.5f);
+		dialog = new Dialog(font, dialogBackground, 65f, 220f);
+		dialog.setPosition(
+		        SCENE_WIDTH/2-dialogBackground.getWidth()*0.5f,
+		        SCENE_HEIGHT/2-dialogBackground.getHeight()*0.5f);
 
+        dialog.addWaitingPage(text);
+
+		stage.addActor(dialog);
+        Gdx.input.setInputProcessor(stage);
     }
 
     @Override
@@ -58,24 +62,15 @@ public class IntroScreen extends AbstractGameScreen {
 		Gdx.gl.glClearColor(0, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
-		updateText();
-		if(Gdx.input.isKeyJustPressed(Keys.R)) stringCompleteness = 0; //กดปุ่ม R เพื่อ Reset ให้ข้อความวิ่งใหม่
-		if(Gdx.input.isKeyJustPressed(Keys.ENTER)) game.setScreen(new GameScreen(game)); //กดปุ่ม R เพื่อ Reset ให้ข้อความวิ่งใหม่
+		if(Gdx.input.isKeyPressed(Keys.ANY_KEY)) {
+		    if (Gdx.input.isKeyPressed(Keys.ENTER))
+                game.setScreen(new GameScreen(game));
+		    else
+		        dialog.tryToChangePage();
+		}
 
-		camera.update();
-	    batch.setProjectionMatrix(camera.combined);
-
-		batch.begin();
-		dialog.draw(batch);
-
-		font.draw(batch, text,
-		        -170f, 60f,           // ตำแหน่ง  x, y
-		        0, charCount,      // start, end ของข้อความที่จะวาด
-		        500f,                 // ความกว้างของข้อความก่อนที่จะ Warp (ตัดขึ้นบรรทัดใหม่)
-		        Align.left,          // การจัดวางข้อความ
-		        true);              // ต้องการให้ Warp ไหม
-
-		batch.end();
+		stage.act(deltaTime);
+        stage.draw();
     }
 
     @Override
@@ -99,15 +94,6 @@ public class IntroScreen extends AbstractGameScreen {
 	    dialogBackground.dispose();
         font.dispose();
 	    batch.dispose();
+        stage.dispose();
     }
-
-	private void updateText() {
-	    stringCompleteness += 30 * Gdx.graphics.getDeltaTime();  // 30 คือ speed ของอักษร
-	    charCount = (int) stringCompleteness;
-	    if (charCount > text.length())
-	    {
-	        charCount = text.length();
-	    }
-	}
-
 }
