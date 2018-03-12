08f662bf2a10f3b428b54cafaccf1bf18bf7b1e1
SanoLFC\sanoth <sanomarutk@gmail.com>
2018-03-13 03:34:46 +0700

    no message

--- core/src/com/mypjgdx/esg/game/objects/items/SolarCell.java
+++ core/src/com/mypjgdx/esg/game/objects/items/SolarCell.java
@@ -7,7 +7,7 @@ import com.mypjgdx.esg.game.objects.characters.Player;
 public class SolarCell extends Item{
 
     public static final float SCALE = 1f;
-    public static final float P_X = 1500f;
+    public static final float P_X = 1550;
     public static final float P_Y = 1100f;
 
 	public SolarCell(TiledMapTileLayer mapLayer, Player player){
