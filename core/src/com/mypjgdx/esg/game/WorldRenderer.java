b456a1b627c3198a047075ccf22d521fe29e7983
SanoLFC\sanothapp <sanomarutk@gmail.com>
2018-03-29 21:51:37 +0700

    add new ui asset, add frame buffer

--- core/src/com/mypjgdx/esg/game/WorldRenderer.java
+++ core/src/com/mypjgdx/esg/game/WorldRenderer.java
@@ -3,12 +3,14 @@ package com.mypjgdx.esg.game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Pixmap;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.glutils.FrameBuffer;
 import com.badlogic.gdx.graphics.glutils.ShaderProgram;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
 import com.badlogic.gdx.utils.Disposable;
-import com.badlogic.gdx.utils.viewport.FitViewport;
+import com.badlogic.gdx.utils.viewport.ExtendViewport;
 
 public class WorldRenderer implements Disposable {
 
@@ -18,7 +20,7 @@ public class WorldRenderer implements Disposable {
     private WorldController worldController; //ส่วนควบคุมเกม
 
     private OrthographicCamera camera;//สร้างตัวแปรกล้อง
-    private FitViewport viewport; //พื้นที่การมอง
+    private ExtendViewport viewport; //พื้นที่การมอง
 
     private SpriteBatch batch; //ตัวแปรการวาด
     private OrthogonalTiledMapRenderer tiledRenderer; // ตัววาด Tiled
@@ -26,6 +28,7 @@ public class WorldRenderer implements Disposable {
 
     private ShaderProgram shader;
     private float[] resolution = new float[2];
+    private FrameBuffer lightFbo;
 
     public WorldRenderer(WorldController worldController) {
         this.worldController = worldController;
@@ -34,11 +37,12 @@ public class WorldRenderer implements Disposable {
 
     private void init () {
         camera = new OrthographicCamera(); //สร้างออปเจ็คกล้องเก็บไว้ในตัวแปร camera
-        viewport = new FitViewport(SCENE_WIDTH, SCENE_HEIGHT, camera); //สร้างออปเจ็คการมองของกล้องเก็บไว้ในตัวแปร
+        viewport = new ExtendViewport(SCENE_WIDTH, SCENE_HEIGHT, camera); //สร้างออปเจ็คการมองของกล้องเก็บไว้ในตัวแปร
 
         batch = new SpriteBatch();//สร้างออปเจ็คไว้วาดสิ่งต่างๆ
         tiledRenderer = new OrthogonalTiledMapRenderer(null,batch);
 
+        lightFbo = new FrameBuffer(Pixmap.Format.RGB888, (int)viewport.getMinWorldWidth(), (int)viewport.getMinWorldHeight(), false);
 
         shapeRenderer = new ShapeRenderer();
         shapeRenderer.setColor(1.0f, 0.0f, 0.0f, 1.0f);
@@ -52,24 +56,49 @@ public class WorldRenderer implements Disposable {
         renderGui();
     }
 
-    private void renderShader() {
-        batch.begin();
-        batch.setBlendFunction(GL20.GL_SRC_ALPHA, GL20.GL_ONE_MINUS_SRC_ALPHA);
-        batch.setColor(0.05f, 0.05f, 0.05f, 1f);
-        batch.draw(Assets.instance.white, 0, 0, viewport.getWorldWidth(), viewport.getWorldHeight());
-        batch.setColor(1, 1, 1, 1);
-        batch.draw(Assets.instance.light, worldController.level.player.getPositionX() - Assets.instance.light.getWidth()/ 2f, worldController.level.player.getPositionY() - Assets.instance.light.getHeight() / 2f);
-        batch.setBlendFunction(GL20.GL_DST_COLOR, GL20.GL_ZERO);
-        batch.end();
-    }
-
     private void renderWorld() {
+        createLightBuffer();
+        viewport.apply();
+
         worldController.cameraHelper.applyTo(camera); //อัพเดทมุมกล้อง
         batch.setProjectionMatrix(viewport.getCamera().combined);
-        renderShader();
         tiledRenderer.setView(camera);
         shapeRenderer.setProjectionMatrix(camera.combined);
         worldController.level.render(batch, tiledRenderer, shapeRenderer); // วาด Game World
+
+        batch.begin();
+        batch.setBlendFunction(GL20.GL_DST_COLOR, GL20.GL_ZERO);
+        batch.draw(lightFbo.getColorBufferTexture(),
+                camera.position.x - camera.viewportWidth * camera.zoom / 2,
+                camera.position.y - camera.viewportHeight * camera.zoom /2,
+                0, 0,
+                lightFbo.getColorBufferTexture().getWidth(), lightFbo.getColorBufferTexture().getHeight(),
+                1 * camera.zoom, 1 * camera.zoom,
+                0,
+                0, 0,
+                lightFbo.getColorBufferTexture().getWidth(), lightFbo.getColorBufferTexture().getHeight(),
+                false, true);
+        batch.setBlendFunction(GL20.GL_SRC_ALPHA, GL20.GL_ONE_MINUS_SRC_ALPHA);
+        batch.end();
+    }
+
+    private void createLightBuffer() {
+        lightFbo.begin();
+
+        Gdx.gl.glClearColor(0.05f, 0.05f, 0.05f, 1f);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+        batch.begin();
+        batch.setColor(1, 1, 1, 1);
+        batch.draw(Assets.instance.light,
+                worldController.level.player.getPositionX() + worldController.level.player.origin.x  -  Assets.instance.light.getWidth()/ 2f,
+                worldController.level.player.getPositionY() + worldController.level.player.origin.y -  Assets.instance.light.getHeight() / 2f);
+        batch.draw(Assets.instance.light,
+                worldController.level.items.get(0).p_x + worldController.level.items.get(0).origin.x - Assets.instance.light.getWidth()/2f,
+                worldController.level.items.get(0).p_y + worldController.level.items.get(0).origin.y - Assets.instance.light.getHeight()/2f);
+        batch.end();
+
+        FrameBuffer.unbind();
     }
 
     private void renderGui() {
@@ -89,6 +118,9 @@ public class WorldRenderer implements Disposable {
         //ตัวสีคือจำนวนของข้อมูล ก็คือ 2 ตัว
         shader.setUniform2fv("resolution", resolution, 0, 2);
         shader.end();
+
+        lightFbo.dispose();
+        lightFbo = new FrameBuffer(Pixmap.Format.RGB888, (int)viewport.getWorldWidth(), (int)viewport.getWorldHeight(), false);
     }
 
     @Override
@@ -96,7 +128,9 @@ public class WorldRenderer implements Disposable {
         tiledRenderer.dispose();
         batch.dispose();
         shader.dispose();
-
+        lightFbo.dispose();
     }
+
+
 
 }
