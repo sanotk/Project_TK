016786efb4edbe0ca9a2895842f211abcdf39081
SanoLFC\sanothapp <sanomarutk@gmail.com>
2018-04-16 14:39:23 +0700

    last link

--- core/src/com/mypjgdx/esg/screens/GameScreen.java
+++ core/src/com/mypjgdx/esg/screens/GameScreen.java
@@ -736,6 +736,7 @@ public class GameScreen extends AbstractGameScreen {
 
     private void removeGuiLink(SolarState solarState){
         for(int i = 0; i < itemLink.etcList.size();i++){
+            System.out.print(itemLink.etcList.get(i).solarState);
             if(itemLink.etcList.get(i).solarState==solarState){
                 itemLink.etcList.remove(i);
             }
