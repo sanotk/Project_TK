72587047082207142bdb8e99ba68a6949a05a471
SanoLFC\sanothapp <sanomarutk@gmail.com>
2018-04-17 23:25:19 +0700

    add map

--- core/src/com/mypjgdx/esg/screens/GameScreen3.java
+++ core/src/com/mypjgdx/esg/screens/GameScreen3.java
@@ -13,7 +13,9 @@ import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.actions.Actions;
 import com.badlogic.gdx.scenes.scene2d.ui.*;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
+import com.badlogic.gdx.scenes.scene2d.utils.NinePatchDrawable;
 import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
+import com.badlogic.gdx.utils.Align;
 import com.badlogic.gdx.utils.viewport.FitViewport;
 import com.mypjgdx.esg.MusicManager;
 import com.mypjgdx.esg.game.Assets;
@@ -23,18 +25,24 @@ import com.mypjgdx.esg.game.levels.Level3;
 import com.mypjgdx.esg.game.levels.Level3Generator;
 import com.mypjgdx.esg.game.objects.characters.Enemy;
 import com.mypjgdx.esg.game.objects.items.Item;
+import com.mypjgdx.esg.utils.ItemLink;
+import com.mypjgdx.esg.utils.SolarState;
 
+import java.util.ArrayList;
+
+@SuppressWarnings("ALL")
 public class GameScreen3 extends AbstractGameScreen {
 
     private WorldController worldController;
     private WorldRenderer worldRenderer;
 
+    private ItemLink itemLink;
+
     SpriteBatch batch;
     public Texture bg;
 
     private Stage stage;
-    private Skin skin;
-
+    private Skin skin;;
 
     public static final int SCENE_WIDTH = 1024; //เซตค่าความกว้างของจอ
     public static final int SCENE_HEIGHT = 576; //เซตค่าความสูงของจอ
@@ -48,13 +56,63 @@ public class GameScreen3 extends AbstractGameScreen {
     private Label energyLevel3;
     private Label textRule;
 
+    public enum systemWindow {
+        solarcell,
+        chargecontroller,
+        battery,
+        inverter
+    }
+
+    public SolarState solarState = null;
+    public systemWindow solarWindow = null;
+
+    private ArrayList<SolarState> link = new ArrayList<SolarState>();
+    private ArrayList<SolarState> isComplete = new ArrayList<SolarState>();
+    private ArrayList<ItemLink> itemLinks = new ArrayList<ItemLink>();
+
+    private TextButton buttonLink1;
+    private TextButton buttonLink2;
+    private TextButton buttonLink3;
+    private TextButton buttonLink4;
+
     private Button buttonOption;
     private BitmapFont font;
     private Window optionsWindow;
 
+    private Window solarcellWindow;
+
     private boolean animation_status = false;
 
-    public GameScreen3(Game game , final Window optionsWindow) {
+    private Button buttonRule;
+    private Window ruleWindow;
+    private Window chartWindow;
+
+    private boolean addedStoC = false;
+    private boolean addedStoB = false;
+    private boolean addedStoI = false;
+    private boolean addedStoD = false;
+    private boolean addedCtoB = false;
+    private boolean addedCtoI = false;
+    private boolean addedCtoD = false;
+    private boolean addedBtoI = false;
+    private boolean addedBtoD = false;
+    private boolean addedItoD = false;
+
+    private float startX;
+    private float startY;
+    private float goalX;
+    private float goalY;
+    private float startWidth;
+    private float startHeight;
+    private float goalWidth;
+    private float goalHeight;
+
+    private int trueLink = 0;
+
+    private TextButton.TextButtonStyle buttonStyle = new TextButton.TextButtonStyle();
+    private TextButton.TextButtonStyle buttonStyle2 = new TextButton.TextButtonStyle();
+
+    public GameScreen3(Game game, final Window optionsWindow) {
         super(game);
 
         stage = new Stage(new FitViewport(SCENE_WIDTH, SCENE_HEIGHT));
@@ -65,25 +123,69 @@ public class GameScreen3 extends AbstractGameScreen {
 
         this.optionsWindow = optionsWindow;
 
-        TextButton.TextButtonStyle buttonStyle = new TextButton.TextButtonStyle();
+        isComplete.add(solarState.StoC);
+        isComplete.add(solarState.CtoB);
+        isComplete.add(solarState.CtoI);
+        isComplete.add(solarState.ItoD);
+
+        TextButton.TextButtonStyle buttonToolStyle = new TextButton.TextButtonStyle();
         TextureRegionDrawable toolUp = new TextureRegionDrawable(Assets.instance.uiBlue.findRegion("icon_tools"));
-        buttonStyle.up = toolUp;
-        buttonStyle.down = toolUp.tint(Color.LIGHT_GRAY);
-        buttonOption = new Button(buttonStyle);
+        buttonToolStyle.up = toolUp;
+        buttonToolStyle.down = toolUp.tint(Color.LIGHT_GRAY);
+        buttonOption = new Button(buttonToolStyle);
         buttonOption.setPosition(SCENE_WIDTH - 50, SCENE_HEIGHT - 50);
+
+        TextButton.TextButtonStyle buttonRuleStyle = new TextButton.TextButtonStyle();
+        TextureRegionDrawable ruleUp = new TextureRegionDrawable(Assets.instance.uiBlue.findRegion("icon_pause"));
+        buttonRuleStyle.up = ruleUp;
+        buttonRuleStyle.down = ruleUp.tint(Color.LIGHT_GRAY);
+        buttonRule = new Button(buttonRuleStyle);
+        buttonRule.setPosition(SCENE_WIDTH - 100, SCENE_HEIGHT - 50);
+
+        buttonStyle.up = new NinePatchDrawable(Assets.instance.uiBlue.createPatch("button_04"));
+        buttonStyle.down = new NinePatchDrawable(Assets.instance.uiBlue.createPatch("button_03"));
+        buttonStyle.font = font;
+
+        buttonStyle2.up = new NinePatchDrawable(Assets.instance.uiRed.createPatch("button_04"));
+        buttonStyle2.down = new NinePatchDrawable(Assets.instance.uiRed.createPatch("button_03"));
+        buttonStyle2.font = font;
+
+        ruleWindow = createRuleWindow();
+        ruleWindow.setPosition(
+                Gdx.graphics.getWidth() / 2 - ruleWindow.getWidth() / 2,
+                Gdx.graphics.getHeight() / 2 - ruleWindow.getHeight() / 2);
+        ruleWindow.addAction(Actions.sequence(Actions.visible(true), Actions.fadeIn(0.2f)));
+
+        chartWindow = createChartWindow();
+        chartWindow.setVisible(false);
 
         optionsWindow.setVisible(false);
 
         stage.addActor(buttonOption);
+        stage.addActor(buttonRule);
+
         stage.addActor(optionsWindow);
+        stage.addActor(ruleWindow);
+        stage.addActor(chartWindow);
 
         buttonOption.addListener(new ClickListener() {
             @Override
             public void clicked(InputEvent event, float x, float y) {
                 optionsWindow.setPosition(
-                        Gdx.graphics.getWidth() / 2 -  optionsWindow.getWidth() / 2,
-                        Gdx.graphics.getHeight() / 2 -  optionsWindow.getHeight() / 2);
+                        Gdx.graphics.getWidth() / 2 - optionsWindow.getWidth() / 2,
+                        Gdx.graphics.getHeight() / 2 - optionsWindow.getHeight() / 2);
                 optionsWindow.addAction(Actions.sequence(Actions.visible(true), Actions.fadeIn(0.2f)));
+            }
+        });
+
+        buttonRule.addListener(new ClickListener() {
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+                ruleWindow.setPosition(
+                        Gdx.graphics.getWidth() / 2 - ruleWindow.getWidth() / 2,
+                        Gdx.graphics.getHeight() / 2 - ruleWindow.getHeight() / 2);
+                ruleWindow.addAction(Actions.sequence(Actions.visible(true), Actions.fadeIn(0.2f)));
+                worldController.level.player.timeStop = true;
             }
         });
 
@@ -91,44 +193,132 @@ public class GameScreen3 extends AbstractGameScreen {
         batch = new SpriteBatch();
     }
 
+    private Window createChartWindow() {
+        Window.WindowStyle style = new Window.WindowStyle();
+        style.background = new NinePatchDrawable(Assets.instance.uiBlue.createPatch("window_01"));
+//        style.background = new TextureRegionDrawable(Assets.instance.uiBlue.findRegion("window_01"));
+        style.titleFont = font;
+        style.titleFontColor = Color.WHITE;
 
-    public  void createbutton() {
+        Label.LabelStyle labelStyle = new Label.LabelStyle();
+        labelStyle.font = font;
+        labelStyle.fontColor = Color.BLACK;
 
-        textBullet = new Label("Bullet Max : " ,skin);
+        Button.ButtonStyle buttonChartStyle = new Button.ButtonStyle();
+        TextureRegionDrawable buttonRegion = new TextureRegionDrawable(Assets.instance.uiBlue.findRegion("button_cross"));
+        buttonChartStyle.up = buttonRegion;
+        buttonChartStyle.down = buttonRegion.tint(Color.LIGHT_GRAY);
+
+        Button closeButton = new Button(buttonChartStyle);
+
+        final Window chartWindow = new Window("Chart", style);
+        chartWindow.setModal(true);
+        chartWindow.padTop(40);
+        chartWindow.padLeft(40);
+        chartWindow.padRight(40);
+        chartWindow.padBottom(20);
+        chartWindow.getTitleLabel().setAlignment(Align.center);
+        chartWindow.row().padBottom(10).padTop(10);
+        chartWindow.row().padTop(10);
+        chartWindow.add(closeButton).colspan(3);
+        chartWindow.pack();
+
+        closeButton.addListener(new ClickListener() {
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+                chartWindow.addAction(Actions.sequence(Actions.fadeOut(0.2f), Actions.visible(false)));
+            }
+        });
+
+        return chartWindow;
+    }
+
+    private Window createRuleWindow() {
+        Window.WindowStyle style = new Window.WindowStyle();
+        style.background = new NinePatchDrawable(Assets.instance.uiBlue.createPatch("window_01"));
+//        style.background = new TextureRegionDrawable(Assets.instance.uiBlue.findRegion("window_01"));
+        style.titleFont = font;
+        style.titleFontColor = Color.WHITE;
+
+        Label.LabelStyle labelStyle = new Label.LabelStyle();
+        labelStyle.font = font;
+        labelStyle.fontColor = Color.BLACK;
+
+        Button.ButtonStyle buttonRuleStyle = new Button.ButtonStyle();
+        TextureRegionDrawable buttonRegion = new TextureRegionDrawable(Assets.instance.uiBlue.findRegion("button_cross"));
+        buttonRuleStyle.up = buttonRegion;
+        buttonRuleStyle.down = buttonRegion.tint(Color.LIGHT_GRAY);
+
+        Button closeButton = new Button(buttonRuleStyle);
+
+        final Window ruleWindow = new Window("Rule", style);
+        ruleWindow.setModal(true);
+        ruleWindow.padTop(40);
+        ruleWindow.padLeft(40);
+        ruleWindow.padRight(40);
+        ruleWindow.padBottom(20);
+        ruleWindow.getTitleLabel().setAlignment(Align.center);
+        ruleWindow.row().padBottom(10).padTop(10);
+        ruleWindow.row().padTop(10);
+        ruleWindow.add(closeButton).colspan(3);
+        ruleWindow.pack();
+
+        closeButton.addListener(new ClickListener() {
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+                ruleWindow.addAction(Actions.sequence(Actions.fadeOut(0.2f), Actions.visible(false)));
+                worldController.level.player.timeStop = false;
+            }
+        });
+
+
+        return ruleWindow;
+    }
+
+    private void createbutton() {
+
+        TextButton.TextButtonStyle buttonStyle = new TextButton.TextButtonStyle();
+        buttonStyle.up = new NinePatchDrawable(Assets.instance.uiBlue.createPatch("button_04"));
+        buttonStyle.down = new NinePatchDrawable(Assets.instance.uiBlue.createPatch("button_03"));
+        buttonStyle.font = font;
+
+        int btn_w = 200;
+        int btn_h = 50;
+
+        textBullet = new Label("Bullet Max : ", skin);
         textBullet.setColor(1, 1, 1, 1);
-        textBullet.setFontScale(1f,1f);
+        textBullet.setFontScale(1f, 1f);
         textBullet.setPosition(50, SCENE_HEIGHT - 50);
 
-        textBeam = new Label("Z-Bullet Max : " ,skin);
+        textBeam = new Label("Z-Bullet Max : ", skin);
         textBeam.setColor(1, 1, 1, 1);
-        textBeam.setFontScale(1.f,1.f);
+        textBeam.setFontScale(1.f, 1.f);
         textBeam.setPosition(200, SCENE_HEIGHT - 50);
 
-        textTrap = new Label("Trap Max : " ,skin);
+        textTrap = new Label("Trap Max : ", skin);
         textTrap.setColor(1, 1, 1, 1);
-        textTrap.setFontScale(1f,1f);
+        textTrap.setFontScale(1f, 1f);
         textTrap.setPosition(350, SCENE_HEIGHT - 50);
 
-        textTime = new Label("Time : " ,skin);
+        textTime = new Label("Time : ", skin);
         textTime.setColor(1, 1, 1, 1);
-        textTime.setFontScale(1f,1f);
+        textTime.setFontScale(1f, 1f);
         textTime.setPosition(450, SCENE_HEIGHT - 100);
 
         energyLevel = new Label("Energy : ", skin);
         energyLevel.setColor(1, 1, 1, 1);
-        energyLevel.setFontScale(1,1f);
+        energyLevel.setFontScale(1, 1f);
         energyLevel.setPosition(500, SCENE_HEIGHT - 50);
 
         energyLevel2 = new Label("Product Energy : ", skin);
         energyLevel2.setColor(1, 1, 1, 1);
-        energyLevel2.setFontScale(1,1f);
+        energyLevel2.setFontScale(1, 1f);
         energyLevel2.setPosition(650, SCENE_HEIGHT - 50);
 
         energyLevel3 = new Label("Battery : ", skin);
         energyLevel3.setColor(1, 1, 1, 1);
-        energyLevel3.setFontScale(1,1f);
+        energyLevel3.setFontScale(1, 1f);
         energyLevel3.setPosition(800, SCENE_HEIGHT - 50);
-
 
         stage.addActor(textBullet);
         stage.addActor(textBeam);
@@ -149,7 +339,7 @@ public class GameScreen3 extends AbstractGameScreen {
         textBeam.setText(String.format("Z-Bullet Max : %d", worldController.level.player.beamCount));
         textTrap.setText(String.format("Trap Max : %d", worldController.level.player.trapCount));
         textTime.setText(String.format("Time limit : %d", worldController.level.player.timeCount));
-        energyLevel.setText(String.format("Energy : %d", (int)worldController.level.energyTube.energy));
+        energyLevel.setText(String.format("Energy : %d", (int) worldController.level.energyTube.energy));
         //
         // sunleft.setText(String.format("Sun Left"));
 
@@ -168,9 +358,10 @@ public class GameScreen3 extends AbstractGameScreen {
             return;
         }
 
-        for(int i = 0; i < worldController.level.enemies.size(); i++){
+
+        for (int i = 0; i < worldController.level.enemies.size(); i++) {
             Enemy enemy = worldController.level.enemies.get(i);
-            if (enemy.dead && !enemy.count){
+            if (enemy.dead && !enemy.count) {
                 worldController.level.energyTube.energy += 2;
                 enemy.count = true;
             }
@@ -206,8 +397,8 @@ public class GameScreen3 extends AbstractGameScreen {
     }
 
     @Override
-    public void pause() {}
-
+    public void pause() {
+    }
 
     private Item findItem(Class clazz) {
         for (int i = 0; i < worldController.level.items.size(); i++) {
