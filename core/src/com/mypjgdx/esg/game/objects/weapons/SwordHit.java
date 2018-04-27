084702da27cf35147e330052a9afcea03c05ad87
SanoLFC\sanothapp <sanomarutk@gmail.com>
2018-04-27 19:50:17 +0700

    edit weapon

--- core/src/com/mypjgdx/esg/game/objects/weapons/SwordHit.java
+++ core/src/com/mypjgdx/esg/game/objects/weapons/SwordHit.java
@@ -2,6 +2,7 @@ package com.mypjgdx.esg.game.objects.weapons;
 
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.maps.tiled.TiledMapTileLayer;
+import com.badlogic.gdx.utils.Timer;
 import com.mypjgdx.esg.game.Assets;
 import com.mypjgdx.esg.game.objects.characters.Damageable;
 import com.mypjgdx.esg.game.objects.characters.Player;
@@ -9,10 +10,12 @@ import com.mypjgdx.esg.game.objects.characters.Player;
 
 public class SwordHit extends Weapon {
 
-    private static final float SCALE = 0.9f;
+    private static final float SCALE = 0.8f;
 
     private static final float INTITAL_FRICTION = 0f;
     private static final float INTITIAL_SPEED = 0f;
+
+    public float timeSinceCollision = 0;
 
     public SwordHit(TiledMapTileLayer mapLayer, Player player) {
         super(Assets.instance.box, SCALE, SCALE, INTITAL_FRICTION, INTITAL_FRICTION);
@@ -30,18 +33,25 @@ public class SwordHit extends Weapon {
         switch (direction) {
             case DOWN:
                 rotation = 270;
+                x -= player.origin.x/2;
+                y -= player.bounds.height/2;
                 velocity.set(0, -INTITIAL_SPEED);
                 break;
             case LEFT:
                 rotation = 180;
                 x -= player.bounds.width;
+                y += 5;
                 velocity.set(-INTITIAL_SPEED, 0);
                 break;
             case RIGHT:
+                x += player.bounds.width/2;
+                y += 5;
                 velocity.set(INTITIAL_SPEED, 0);
                 break;
             case UP:
                 rotation = 90;
+                x -= player.origin.x/2;
+                y += player.bounds.height/2;
                 velocity.set(0, INTITIAL_SPEED);
                 break;
             default:
@@ -50,6 +60,8 @@ public class SwordHit extends Weapon {
         setPosition(
                 x,
                 y);
+
+        delay();
     }
 
     @Override
@@ -72,6 +84,16 @@ public class SwordHit extends Weapon {
                 break;
         }
         destroy();
+    }
+
+    public void delay(){
+        float delay = 0.5f; // seconds
+        Timer.schedule(new Timer.Task(){
+            @Override
+            public void run() {
+                destroy();
+            }
+        }, delay);
     }
 
     public void debug(ShapeRenderer renderer) {
