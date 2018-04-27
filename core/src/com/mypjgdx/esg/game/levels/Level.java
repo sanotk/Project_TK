9ac167cc920162a357fb16fcc1b18074bdc0caf6
SanoLFC\sanothapp <sanomarutk@gmail.com>
2018-04-27 20:47:10 +0700

    edit render citizen

--- core/src/com/mypjgdx/esg/game/levels/Level.java
+++ core/src/com/mypjgdx/esg/game/levels/Level.java
@@ -79,7 +79,7 @@ public abstract class Level {
         for (Etc e : etcs) e.render(batch);
         for (Item i : items) i.render(batch);
         for (Enemy e : enemies) e.render(batch);
-        System.out.print(player.stageoneclear);
+        //System.out.print(player.stageoneclear);
         if (player.stageoneclear) {
             for (Citizen c : citizens) c.render(batch);
         }
