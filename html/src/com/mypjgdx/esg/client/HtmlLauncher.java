21fa91be0c86adec223fb7abf3014661314dc030
skyousuke <surasek@gmail.com>
2018-04-30 14:33:12 +0700

    update project structure

---
+++ html/src/com/mypjgdx/esg/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package com.mypjgdx.esg.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import com.mypjgdx.esg.MyPjGdxGame;
+
+public class HtmlLauncher extends GwtApplication {
+
+    @Override
+    public GwtApplicationConfiguration getConfig() {
+        return new GwtApplicationConfiguration(1024, 576);
+    }
+
+    @Override
+    public ApplicationListener createApplicationListener() {
+        return new MyPjGdxGame();
+    }
+}
