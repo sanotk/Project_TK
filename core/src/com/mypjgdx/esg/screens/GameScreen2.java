b66c3ddd85de330a80aa6c0ffe6144928edd24e0
sano <sanomarutk@gmail.com>
2018-05-17 02:13:39 +0700

    edit lv1 solarCell

--- core/src/com/mypjgdx/esg/screens/GameScreen2.java
+++ core/src/com/mypjgdx/esg/screens/GameScreen2.java
@@ -1052,16 +1052,17 @@ public class GameScreen2 extends AbstractGameScreen {
                 break;
             }
         }
-
-        for (Item item : level2.items) {
-            if ((player.status_find) && item.nearPlayer() && item.state == Item.ItemState.ONLOOP) {
-                item.state = Item.ItemState.OFF;
-                EnergyUsedBar.instance.energyUse -= item.getEnergyBurn();
-                player.status_find = false;
-            } else if ((player.status_find) && item.nearPlayer() && item.state == Item.ItemState.OFF) {
-                item.state = Item.ItemState.ONLOOP;
-                EnergyUsedBar.instance.energyUse += item.getEnergyBurn();
-                player.status_find = false;
+        if((stageTwoClear) && (player.status_find)){
+            for (Item item : level2.items) {
+                if (item.nearPlayer() && item.state == Item.ItemState.ONLOOP) {
+                    item.state = Item.ItemState.OFF;
+                    EnergyUsedBar.instance.energyUse -= item.getEnergyBurn();
+                    player.status_find = false;
+                } else if ((player.status_find) && item.nearPlayer() && item.state == Item.ItemState.OFF) {
+                    item.state = Item.ItemState.ONLOOP;
+                    EnergyUsedBar.instance.energyUse += item.getEnergyBurn();
+                    player.status_find = false;
+                }
             }
         }
 
