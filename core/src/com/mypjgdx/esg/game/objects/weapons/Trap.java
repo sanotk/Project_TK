185bdd2011ae6936af301bbb4ad3a6aa1d7340e5
sano <sanomarutk@gmail.com>
2018-05-30 22:12:36 +0700

    แก้ไขปุ่ม

--- core/src/com/mypjgdx/esg/game/objects/weapons/Trap.java
+++ core/src/com/mypjgdx/esg/game/objects/weapons/Trap.java
@@ -8,37 +8,37 @@ import com.mypjgdx.esg.game.objects.characters.Damageable;
 import com.mypjgdx.esg.game.objects.characters.Player;
 
 
-public class Trap extends Weapon implements Json.Serializable{
+public class Trap extends Weapon implements Json.Serializable {
 
-	    private static final float SCALE = 0.75f;
+    private static final float SCALE = 0.75f;
 
-	    private static final float INTITAL_FRICTION = 50f;
-	    private static final float INTITIAL_SPEED = 100f;
-	    private int damageCount = 0;
+    private static final float INTITAL_FRICTION = 50f;
+    private static final float INTITIAL_SPEED = 100f;
+    private int damageCount = 0;
 
-	    public Trap(TiledMapTileLayer mapLayer ,Player player) {
-	        super(Assets.instance.trap, SCALE, SCALE, INTITAL_FRICTION, INTITAL_FRICTION);
-            init(mapLayer ,player, enemy);
-	    }
+    public Trap(TiledMapTileLayer mapLayer, Player player) {
+        super(Assets.instance.trap, SCALE, SCALE, INTITAL_FRICTION, INTITAL_FRICTION);
+        init(mapLayer, player, enemy);
+    }
 
-        @Override
-        protected void spawn() {
-            setPosition(
-                    player.getPositionX() + player.origin.x - origin.x,
-                    player.getPositionY() + player.origin.y - 9);
+    @Override
+    protected void spawn() {
+        setPosition(
+                player.getPositionX() + player.origin.x - origin.x,
+                player.getPositionY() + player.origin.y - 9);
 
-            direction = player.getViewDirection();
+        direction = player.getViewDirection();
 
-            switch(direction){
+        switch (direction) {
             case DOWN:
                 rotation = 90;
-                velocity.set(0,-INTITIAL_SPEED);
+                velocity.set(0, -INTITIAL_SPEED);
                 break;
             case LEFT:
-                velocity.set(-INTITIAL_SPEED,0);
+                velocity.set(-INTITIAL_SPEED, 0);
                 break;
             case RIGHT:
-                velocity.set(INTITIAL_SPEED,0);
+                velocity.set(INTITIAL_SPEED, 0);
                 break;
             case UP:
                 rotation = 90;
@@ -46,38 +46,57 @@ public class Trap extends Weapon implements Json.Serializable{
                 break;
             default:
                 break;
-            }
         }
+    }
 
-        @Override
-        public void attack(Damageable damageable) {
-            float knockbackSpeed = 500f;
-            switch(damageable.getViewDirection()) {
-            case DOWN: damageable.takeDamage(1, knockbackSpeed, 90); break;
-            case LEFT: damageable.takeDamage(1, knockbackSpeed, 0); break;
-            case RIGHT: damageable.takeDamage(1, knockbackSpeed, 180); break;
-            case UP:  damageable.takeDamage(1, knockbackSpeed, 270); break;
-            default: break;
-            }
-            this.damageCount += 1;
-            if(damageCount==3){
-                destroy();
-            }
+    @Override
+    public void attack(Damageable damageable) {
+        float knockbackSpeed = 500f;
+        switch (damageable.getViewDirection()) {
+            case DOWN:
+                damageable.takeDamage(1, knockbackSpeed, 90);
+                break;
+            case LEFT:
+                damageable.takeDamage(1, knockbackSpeed, 0);
+                break;
+            case RIGHT:
+                damageable.takeDamage(1, knockbackSpeed, 180);
+                break;
+            case UP:
+                damageable.takeDamage(1, knockbackSpeed, 270);
+                break;
+            default:
+                break;
         }
+        this.damageCount += 1;
+        if (damageCount == 3) {
+            destroy();
+        }
+    }
 
-        @Override
-        public String getName() {
-            // TODO Auto-generated method stub
-            return null;
-        }
+    @Override
+    public String getName() {
+        // TODO Auto-generated method stub
+        return null;
+    }
 
     @Override
     public void write(Json json) {
-	        json.writeValue("damageCount", damageCount);
+        json.writeValue("damageCount", damageCount);
     }
 
     @Override
     public void read(Json json, JsonValue jsonData) {
         damageCount = (int) jsonData.get("Trap").getFloat("damageCount");
+    }
+
+    @Override
+    public float getPositionX() {
+        return super.getPositionX();
+    }
+
+    @Override
+    public float getPositionY() {
+        return super.getPositionY();
     }
 }
