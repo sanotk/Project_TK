fdf479f168f5023be024981285f7495a28f8afa3
sano <sanomarutk@gmail.com>
2018-06-14 17:16:46 +0700

    edit game screen 1

--- core/src/com/mypjgdx/esg/game/levels/Level3.java
+++ core/src/com/mypjgdx/esg/game/levels/Level3.java
@@ -32,6 +32,15 @@ public class Level3 extends Level {
     public Item refrigerator;
     public Item riceCooker;
     public Item gate;
+    public Item lamp1;
+    public Item lamp2;
+    public Item lamp3;
+    public Item lamp4;
+    public Item lamp5;
+    public Item lamp6;
+    public Item lamp7;
+    public Item lamp8;
+    public Item lamp9;
 
 
     public Level3() {
@@ -52,6 +61,15 @@ public class Level3 extends Level {
         refrigerator = new Refrigerator(mapLayer, player,650f,1000f);
         riceCooker = new RiceCooker(mapLayer, player,750,1050f);
         gate = new Gate(mapLayer,player);
+        lamp1 = new Lamp(mapLayer, player, 100f, 1150f);
+        lamp2 = new Lamp(mapLayer, player, 400f, 1150f);
+        lamp3 = new Lamp(mapLayer, player, 700f, 1150f);
+        lamp4 = new Lamp(mapLayer, player, 1000f, 1150f);
+        lamp5 = new Lamp(mapLayer, player, 1300f, 1150f);
+        lamp6 = new Lamp(mapLayer, player, 100f, 750f);
+        lamp7 = new Lamp(mapLayer, player, 400f, 750f);
+        lamp8 = new Lamp(mapLayer, player, 900f, 200f);
+        lamp9 = new Lamp(mapLayer, player, 1200f, 200f);
 
         items.add(switchItem);
         items.add(television);
@@ -63,6 +81,15 @@ public class Level3 extends Level {
         items.add(refrigerator);
         items.add(riceCooker);
         items.add(gate);
+        items.add(lamp1);
+        items.add(lamp2);
+        items.add(lamp3);
+        items.add(lamp4);
+        items.add(lamp5);
+        items.add(lamp6);
+        items.add(lamp7);
+        items.add(lamp8);
+        items.add(lamp9);
 
         enemies.add(new Pepo(mapLayer, player));
         enemies.add(new Pepo(mapLayer, player));
@@ -112,6 +139,15 @@ public class Level3 extends Level {
         fan1.setEnergyBurn(60);
         refrigerator.setEnergyBurn(150);
         riceCooker.setEnergyBurn(800);
+        lamp1.setEnergyBurn(28);
+        lamp2.setEnergyBurn(28);
+        lamp3.setEnergyBurn(28);
+        lamp4.setEnergyBurn(28);
+        lamp5.setEnergyBurn(28);
+        lamp6.setEnergyBurn(28);
+        lamp7.setEnergyBurn(28);
+        lamp8.setEnergyBurn(28);
+        lamp9.setEnergyBurn(28);
     }
 
     @Override
