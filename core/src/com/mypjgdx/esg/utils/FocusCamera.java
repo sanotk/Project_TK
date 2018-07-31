8578ded7f683cefc0ab5aa6468c44c728b4c47d5
sano <sanomarutk@gmail.com>
2018-08-01 04:50:31 +0700

    editfocus camera

--- core/src/com/mypjgdx/esg/utils/FocusCamera.java
+++ core/src/com/mypjgdx/esg/utils/FocusCamera.java
@@ -1,9 +1,18 @@
 package com.mypjgdx.esg.utils;
 
 public class FocusCamera {
-    public boolean focus1;
+
+    private boolean focus1;
+//    private boolean focus2;
+//    private boolean focus3;
+//    private boolean focus4;
+//    private boolean focus5;
 
     public void setFocus1(boolean focus1){
         focus1 = focus1;
+    }
+
+    public boolean getFocus1(){
+        return focus1;
     }
 }
