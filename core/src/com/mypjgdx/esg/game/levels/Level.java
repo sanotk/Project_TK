395d85ae48bb39d33b7c1aa3375cd88d9bbdf50b
sano <sanomarutk@gmail.com>
2018-11-05 00:43:22 +0700

    game 1

--- core/src/com/mypjgdx/esg/game/levels/Level.java
+++ core/src/com/mypjgdx/esg/game/levels/Level.java
@@ -67,7 +67,7 @@ public abstract class Level implements Json.Serializable {
         for (Item i : items) i.render(batch);
         for (Enemy e : enemies) e.render(batch);
 //        if (player.stageOneClear) {
-//            
+//
 //        }
         for (Citizen c : citizens) c.render(batch);
         player.render(batch);
@@ -122,9 +122,10 @@ public abstract class Level implements Json.Serializable {
         }
         player.update(deltaTime, citizens);
         for (Link link : links) link.update(deltaTime);
-        if (player.stageOneClear) {
-            for (Citizen c : citizens) c.update(deltaTime);
-        }
+//        if (player.stageOneClear) {
+//           
+//        }
+        for (Citizen c : citizens) c.update(deltaTime);
         if(!player.timeStop){
             for (Weapon w : weapons) w.update(deltaTime);
             for (DroppedItem droppedItem : droppedItems) droppedItem.update(deltaTime);
