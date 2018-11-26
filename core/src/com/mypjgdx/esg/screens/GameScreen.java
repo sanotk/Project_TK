8026abe5deb4cecd44b96455848ea5ce666e7ff1
sano <sanomarutk@gmail.com>
2018-11-26 20:35:47 +0700

    game 1

--- core/src/com/mypjgdx/esg/screens/GameScreen.java
+++ core/src/com/mypjgdx/esg/screens/GameScreen.java
@@ -272,7 +272,7 @@ public class GameScreen extends AbstractGameScreen {
         isComplete.add(QuestState.quest6no);
 
         skin = new Skin(Gdx.files.internal("uiskin.json"));
-        bg = new Texture("bg.png");
+        bg = new Texture("bg1.png");
         font = Assets.instance.newFont;
         font.getRegion().getTexture().setFilter(Texture.TextureFilter.Linear, Texture.TextureFilter.Linear);
         font.setColor(Color.WHITE);
